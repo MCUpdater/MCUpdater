dd54f7b14e790c17dac028a865452db8a5498cc6
Scott M. Barbour <smbarbour@gmail.com>
2012-03-15 18:17:32 -0500

    Added ServerPack.xsd

--- MCU-API/src/cah/melonar/mcu/MCUApp.java
+++ MCU-API/src/cah/melonar/mcu/MCUApp.java
@@ -2,14 +2,7 @@ package cah.melonar.mcu;
 
 public abstract class MCUApp {
 
-	public void setLblStatus(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setProgressBar(int i) {
-		// TODO Auto-generated method stub
-		
-	}
+	public abstract void setLblStatus(String string);
+	public abstract void setProgressBar(int i);
 
 }
