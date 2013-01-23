16d1985e87baf7a549b20a2d4ce0cab86e72bde1
Scott M. Barbour <smbarbour@gmail.com>
2013-01-23 07:39:25 -0600

    Tweaked versioning to make built filename include branch and version data

--- MCU-API/src/org/smbarbour/mcu/Version.java
+++ MCU-API/src/org/smbarbour/mcu/Version.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Version {
+	public static final int MAJOR_VERSION;
+	public static final int MINOR_VERSION;
 	public static final int BUILD_VERSION;
 	public static final String BUILD_BRANCH;
 	public static final String BUILD_LABEL;
@@ -13,6 +15,8 @@ public class Version {
 			prop.load(Version.class.getResourceAsStream("/version.properties"));
 		} catch (IOException e) {
 		}
+		MAJOR_VERSION = Integer.valueOf(prop.getProperty("major","0"));
+		MINOR_VERSION = Integer.valueOf(prop.getProperty("minor","0"));
 		BUILD_VERSION = Integer.valueOf(prop.getProperty("build_version","0"));
 		BUILD_BRANCH = prop.getProperty("git_branch","unknown");
 		if( BUILD_BRANCH.equals("unknown") || BUILD_BRANCH.equals("master") ) {
@@ -22,8 +26,6 @@ public class Version {
 		}
 	}
 	
-	public static final int MAJOR_VERSION = 2;
-	public static final int MINOR_VERSION = 2;
 	public static final String API_VERSION = MAJOR_VERSION + "." + MINOR_VERSION;
 	public static final String VERSION = "v"+MAJOR_VERSION+"."+MINOR_VERSION+"."+BUILD_VERSION;
 	
