50f4018518345ba181590ee089bf63710395834a
Scott M. Barbour <smbarbour@gmail.com>
2013-06-19 14:01:00 -0500

    More work on the new SWT gui. (Panel resizing!)

--- MCU-EndUser/src/org/smbarbour/mcu/EndUser/MainShell.java
+++ MCU-EndUser/src/org/smbarbour/mcu/EndUser/MainShell.java
@@ -2,17 +2,23 @@ package org.smbarbour.mcu.EndUser;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import swing2swt.layout.BorderLayout;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Sash;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 
@@ -55,11 +61,134 @@ public class MainShell {
 		shell = new Shell();
 		shell.setSize(1175, 592);
 		shell.setText("MCUpdater 3.1.0");
-		shell.setLayout(new BorderLayout(0, 0));
+		shell.setLayout(new GridLayout(1,false));
+		
+		shell.addListener(SWT.RESIZE, new Listener() {
+
+			@Override
+			public void handleEvent(Event arg0) {
+				shell.layout();
+			}
+		});
+		
+		final Composite mainArea = new Composite(shell, SWT.NONE);
+		mainArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		mainArea.setLayout(new FormLayout());
+		{
+			final int limit = 20;
+			final Sash sashLeft = new Sash(mainArea, SWT.VERTICAL);
+			final Sash sashRight = new Sash(mainArea, SWT.VERTICAL);
+
+			Group grpInstances = new Group(mainArea, SWT.V_SCROLL);
+			grpInstances.setText("Instances");
+			grpInstances.setLayout(new RowLayout());
+			FormData grpInstancesData = new FormData();
+			{
+				grpInstancesData.left = new FormAttachment(0,0);
+				grpInstancesData.right = new FormAttachment(sashLeft, 0);
+				grpInstancesData.top = new FormAttachment(0,0);
+				grpInstancesData.bottom = new FormAttachment(100,0);
+			}
+			grpInstances.setLayoutData(grpInstancesData);			
+			{
+				Label lblTest1 = new Label(grpInstances, SWT.NONE);
+				lblTest1.setText("First instance in the list.");
+				lblTest1.setSize(lblTest1.computeSize(400, SWT.DEFAULT));
+			
+				grpInstances.pack();
+			}
+			final FormData sashLeftData = new FormData();
+			{
+				sashLeftData.left = new FormAttachment(20, 0);
+				sashLeftData.top = new FormAttachment(0, 0);
+				sashLeftData.bottom = new FormAttachment(100, 0);
+			}
+			sashLeft.setLayoutData(sashLeftData);
+			sashLeft.addListener(SWT.Selection, new Listener(){
+				@Override
+				public void handleEvent(Event e) {
+					Rectangle sashRect = sashLeft.getBounds();
+					Rectangle viewRect = mainArea.getClientArea();
+					int right = viewRect.width - sashRect.width - limit;
+					e.x = Math.max(Math.min(e.x, right), limit);
+					if (e.x != sashRect.x) {
+						sashLeftData.left = new FormAttachment(0, e.x);
+						mainArea.layout();
+					}
+				}
+			});
+			
+			final TabFolder tabFolder = new TabFolder(mainArea, SWT.NONE);
+			final FormData grpTabData = new FormData();
+			{
+				grpTabData.left = new FormAttachment(sashLeft,0);
+				grpTabData.right = new FormAttachment(sashRight,0);
+				grpTabData.top = new FormAttachment(0,0);
+				grpTabData.bottom = new FormAttachment(100,0);
+			}
+			tabFolder.setLayoutData(grpTabData);
+			{
+				TabItem tbtmNews = new TabItem(tabFolder, SWT.V_SCROLL);
+				tbtmNews.setText("News");
+
+				Browser browser = new Browser(tabFolder, SWT.NONE);
+				browser.setUrl("http://files.mcupdater.com/example/SamplePack.xml");
+				tbtmNews.setControl(browser);
+
+				TabItem tbtmConsole = new TabItem(tabFolder, SWT.NONE);
+				tbtmConsole.setText("Console");
+
+				TabItem tbtmSettings = new TabItem(tabFolder, SWT.NONE);
+				tbtmSettings.setText("Settings");
+
+				SettingsPanel cmpSettings = new SettingsPanel(tabFolder, SWT.NONE);
+				tbtmSettings.setControl(cmpSettings);
+			}
+			
+			Group grpModules = new Group(mainArea, SWT.V_SCROLL);
+			grpModules.setText("Modules");
+			final FormData grpModulesData = new FormData();
+			{
+				grpModulesData.left = new FormAttachment(sashRight,0);
+				grpModulesData.right = new FormAttachment(100, 0);
+				grpModulesData.top = new FormAttachment(0,0);
+				grpModulesData.bottom = new FormAttachment(100,0);
+			}
+			grpModules.setLayoutData(grpModulesData);
+			grpModules.setLayout(new RowLayout());
+			{
+				Button btnTest2 = new Button(grpModules, SWT.CHECK);
+				btnTest2.setText("Minecraft Forge");
+				btnTest2.setSelection(true);
+				btnTest2.setSize(btnTest2.computeSize(500, SWT.DEFAULT));
+				grpModules.pack();
+			}
+			
+			final FormData sashRightData = new FormData();
+			{
+				sashRightData.right = new FormAttachment(80,0);
+				sashRightData.top = new FormAttachment(0, 0);
+				sashRightData.bottom = new FormAttachment(100, 0);
+			}
+			sashRight.setLayoutData(sashRightData);
+			sashRight.addListener(SWT.Selection, new Listener(){
+				@Override
+				public void handleEvent(Event e) {
+					Rectangle sashRect = sashRight.getBounds();
+					Rectangle viewRect = mainArea.getClientArea();
+					int right = viewRect.width - sashRect.width - limit;
+					e.x = Math.max(Math.min(e.x, right), limit);
+					if (e.x != sashRect.x) {
+						sashRightData.right = new FormAttachment(0, e.x);
+						mainArea.layout();
+					}
+				}
+			});
+		}
 		
 		Composite cmpStatus = new Composite(shell, SWT.NONE);
+		cmpStatus.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
 		{
-			cmpStatus.setLayoutData(BorderLayout.SOUTH);
 			cmpStatus.setLayout(new GridLayout(4, false));
 			
 			Label lblStatus = new Label(cmpStatus, SWT.NONE);
@@ -78,45 +207,5 @@ public class MainShell {
 			btnLaunch.setLayoutData(new GridData(SWT.LEFT,SWT.TOP,false,false,1,1));
 			btnLaunch.setText("Launch Minecraft");
 		}
-		
-		Group grpInstances = new Group(shell, SWT.V_SCROLL);
-		grpInstances.setText("Instances");
-		grpInstances.setLayoutData(BorderLayout.WEST);
-		grpInstances.setLayout(new RowLayout());
-		
-		Label lblTest1 = new Label(grpInstances, SWT.NONE);
-		lblTest1.setText("First instance in the list.");
-		lblTest1.setSize(lblTest1.computeSize(400, SWT.DEFAULT));
-		grpInstances.pack();
-				
-		Group grpModules = new Group(shell, SWT.V_SCROLL);
-		grpModules.setText("Modules");
-		grpModules.setLayoutData(BorderLayout.EAST);
-		grpModules.setLayout(new RowLayout());
-		
-		Button btnTest2 = new Button(grpModules, SWT.CHECK);
-		btnTest2.setText("Minecraft Forge");
-		btnTest2.setSelection(true);
-		btnTest2.setSize(btnTest2.computeSize(500, SWT.DEFAULT));
-		grpModules.pack();
-		
-		TabFolder tabFolder = new TabFolder(shell, SWT.NONE);
-		tabFolder.setLayoutData(BorderLayout.CENTER);
-		
-		TabItem tbtmNews = new TabItem(tabFolder, SWT.V_SCROLL);
-		tbtmNews.setText("News");
-		
-		Browser browser = new Browser(tabFolder, SWT.NONE);
-		browser.setUrl("http://files.mcupdater.com/example/SamplePack.xml");
-		tbtmNews.setControl(browser);
-		
-		TabItem tbtmConsole = new TabItem(tabFolder, SWT.NONE);
-		tbtmConsole.setText("Console");
-
-		TabItem tbtmSettings = new TabItem(tabFolder, SWT.NONE);
-		tbtmSettings.setText("Settings");
-		
-		SettingsPanel cmpSettings = new SettingsPanel(tabFolder, SWT.NONE);
-		tbtmSettings.setControl(cmpSettings);
 	}
 }
--- MCU-EndUser/src/org/smbarbour/mcu/EndUser/SettingsPanel.java
+++ MCU-EndUser/src/org/smbarbour/mcu/EndUser/SettingsPanel.java
@@ -5,8 +5,10 @@ import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Text;
 
 public class SettingsPanel extends Composite {
@@ -14,9 +16,9 @@ public class SettingsPanel extends Composite {
 	private ScrolledComposite scroller;
 	private Composite content;
 	private GridLayout glContent = new GridLayout(3,false);
-	final private GridData gdLabel = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
-	final private GridData gdFillSpan = new GridData(SWT.FILL, SWT.TOP, true, false, 2, 1);
-	final private GridData gdFullSpan = new GridData(SWT.FILL, SWT.TOP, true, false, 3, 1);
+//	final private GridData gdLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+//	final private GridData gdFillSpan = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+//	final private GridData gdFullSpan = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
 
 	public SettingsPanel(Composite parent, int style) {
 		super(parent, style);
@@ -33,51 +35,132 @@ public class SettingsPanel extends Composite {
 	}
 
 	private void buildPanel() {
-		int maxWidth = 0;
-		
-		Label lblMinMem = new Label(content, SWT.NONE);
-		lblMinMem.setLayoutData(gdLabel);
-		lblMinMem.setText("Minimum Memory:");
-		lblMinMem.setAlignment(SWT.RIGHT);
-		maxWidth = lblMinMem.computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
-		
-		Text txtMinMem = new Text(content, SWT.FILL);
-		txtMinMem.setLayoutData(gdFillSpan);
-		txtMinMem.setText("");
+		{
+			Label lblMinMem = new Label(content, SWT.NONE);
+			lblMinMem.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblMinMem.setText("Minimum Memory:");
+			lblMinMem.setAlignment(SWT.RIGHT);
 
-		Label lblMaxMem = new Label(content, SWT.NONE);
-		lblMaxMem.setLayoutData(gdLabel);
-		lblMaxMem.setText("Maximum Memory:");
-		lblMaxMem.setAlignment(SWT.RIGHT);
-		maxWidth = Math.max(maxWidth, lblMaxMem.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
-		
-		Text txtMaxMem = new Text(content, SWT.FILL);
-		txtMaxMem.setLayoutData(gdFillSpan);
-		txtMaxMem.setText("");
+			Text txtMinMem = new Text(content, SWT.FILL);
+			txtMinMem.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			txtMinMem.setText("");
+		}
+		{
+			Label lblMaxMem = new Label(content, SWT.NONE);
+			lblMaxMem.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblMaxMem.setText("Maximum Memory:");
+			lblMaxMem.setAlignment(SWT.RIGHT);
 
-		Label lblPermGen = new Label(content, SWT.NONE);
-		lblPermGen.setLayoutData(gdLabel);
-		lblPermGen.setText("PermGen Space:");
-		lblPermGen.setAlignment(SWT.RIGHT);
-		maxWidth = Math.max(maxWidth, lblPermGen.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
-		
-		Text txtPermGen = new Text(content, SWT.FILL);
-		txtPermGen.setLayoutData(gdFillSpan);
-		txtPermGen.setText("");
+			Text txtMaxMem = new Text(content, SWT.FILL);
+			txtMaxMem.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			txtMaxMem.setText("");
+		}
+		{
+			Label lblPermGen = new Label(content, SWT.NONE);
+			lblPermGen.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblPermGen.setText("PermGen Space:");
+			lblPermGen.setAlignment(SWT.RIGHT);
 
-		Label lblMemNotice = new Label(content, SWT.NONE);
-		lblMemNotice.setLayoutData(gdFullSpan);
-		lblMemNotice.setText("Memory can be specified in MB or GB (i.e. 512M or 1G).\nIncreasing memory may help performance, but often has no measurable impact.");
-		lblMemNotice.setAlignment(SWT.CENTER);
-		
-		Label lblResolution = new Label(content, SWT.NONE);
-		lblResolution.setLayoutData(gdLabel);
-		lblResolution.setText("Resolution:");
-		lblResolution.setAlignment(SWT.RIGHT);
-		maxWidth = Math.max(maxWidth, lblResolution.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
-		
-		gdLabel.widthHint = maxWidth;
+			Text txtPermGen = new Text(content, SWT.FILL);
+			txtPermGen.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			txtPermGen.setText("");
+		}
+		{
+			Label lblMemNotice = new Label(content, SWT.NONE);
+			lblMemNotice.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1));
+			lblMemNotice.setText("Memory can be specified in MB or GB (i.e. 512M or 1G).\nIncreasing memory may help performance, but often has no measurable impact.");
+			lblMemNotice.setAlignment(SWT.CENTER);
+		}
+		{
+			Label lblResolution = new Label(content, SWT.NONE);
+			lblResolution.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblResolution.setText("Resolution:");
+			lblResolution.setAlignment(SWT.RIGHT);
+
+			Composite resPanel = new Composite(content, SWT.NONE);
+			resPanel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			resPanel.setLayout(new GridLayout(3,false));
+			{
+				Text txtResWidth = new Text(resPanel, SWT.NONE);
+				txtResWidth.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+				txtResWidth.setText("");
+
+				Label lblResSeparator = new Label(resPanel, SWT.NONE);
+				lblResSeparator.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+				lblResSeparator.setText("X");
+
+				Text txtResHeight = new Text(resPanel, SWT.NONE);
+				txtResHeight.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+				txtResHeight.setText("");
+			}
+		}
+		{
+			Label lblJavaHome = new Label(content, SWT.NONE);
+			lblJavaHome.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblJavaHome.setText("Java home path:");
+
+			Text txtJavaHome = new Text(content, SWT.NONE);
+			txtJavaHome.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+			txtJavaHome.setText("");
+
+			Button btnJavaHomeBrowse = new Button(content, SWT.PUSH);
+			btnJavaHomeBrowse.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+			btnJavaHomeBrowse.setText("Browse");
+		}
+		{
+			Label lblJVMOpts = new Label(content, SWT.NONE);
+			lblJVMOpts.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblJVMOpts.setText("JVM Opts:");
+
+			Text txtJVMOpts = new Text(content, SWT.NONE);
+			txtJVMOpts.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			txtJVMOpts.setText("");
+		}
+		{
+			Label lblInstanceRoot = new Label(content, SWT.NONE);
+			lblInstanceRoot.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblInstanceRoot.setText("Instance folder:");
+			
+			Text txtInstanceRoot = new Text(content, SWT.NONE);
+			txtInstanceRoot.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+			txtInstanceRoot.setText("");
+			
+			Button btnInstanceRootBrowse = new Button(content, SWT.PUSH);
+			btnInstanceRootBrowse.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+			btnInstanceRootBrowse.setText("Browse");
+		}
+		{
+			Label lblWrapper = new Label(content, SWT.NONE);
+			lblWrapper.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblWrapper.setText("Program wrapper:");
+			
+			Text txtWrapper = new Text(content, SWT.NONE);
+			txtWrapper.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+			txtWrapper.setText("");
+		}
+		{
+			Label lblAutoMinimize = new Label(content, SWT.NONE);
+			lblAutoMinimize.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblAutoMinimize.setText("Minimize on launch:");
+			
+			Button chkAutoMinimize = new Button(content, SWT.CHECK);
+			chkAutoMinimize.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		}
+		{
+			Label lblAutoConnect = new Label(content, SWT.NONE);
+			lblAutoConnect.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			lblAutoConnect.setText("Automatically connect:");
+			
+			Button chkAutoConnect = new Button(content, SWT.CHECK);
+			chkAutoConnect.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		}
+		{
+			Label lblPackList = new Label(content, SWT.NONE);
+			lblPackList.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false));
+			lblPackList.setText("Defined Pack URLs:");
+			
+			List lstPackList = new List(content, SWT.V_SCROLL);
+			lstPackList.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 6));
+		}
 	}
-
-	
 }
