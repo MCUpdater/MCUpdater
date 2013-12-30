22bc7c010d203cff6ba80297709b90d4bc1586db
Ammon Lauritzen <allaryin@gmail.com>
2013-12-30 15:42:08 -0600

    Added support for parsing imports.

--- MCU-CLI-server/php/mcu-cli.php
+++ MCU-CLI-server/php/mcu-cli.php
@@ -60,31 +60,101 @@ function identify_server($server, $id) {
 		return $server;
 	return false;
 }
-
-if( !$pack->Server ) {
-	msg("Unable to find any <Server/> directive in xml", true);
-	exit(2);
-}
-if( is_array($pack->Server) ) {
-	foreach ($pack->Server as $key => $val) {
-		$base = identify_server($val, $pack_server_id);
-		if( $base ) 
-			break;
+function find_server($pack, $server_id) {
+	if( !$pack->Server ) {
+		msg("Unable to find any <Server/> directive in xml", true);
+		return false;
 	}
-} else {
-	$base = identify_server($pack->Server, $pack_server_id);
+	if( is_array($pack->Server) ) {
+		foreach ($pack->Server as $key => $val) {
+			$base = identify_server($val, $server_id);
+			if( $base ) 
+				break;
+		}
+	} else {
+		$base = identify_server($pack->Server, $server_id);
+	}
+	return $base;
 }
+$base = find_server($pack, $pack_server_id);
 if( !$base ) {
 	msg("Unable to find server id $pack_server_id", true);
 	exit(2);
 }
+//print_r($base);
+
+// import all imports
+function append_children($base, $child) {
+	$dom_base = dom_import_simplexml($base);
+	foreach( $child->children() as $key => $val ) {
+		$dom_child = dom_import_simplexml($val);
+		$dom_child = $dom_base->ownerDocument->importNode($dom_child, TRUE);
+		$dom_base->appendChild($dom_child);
+	}
+}
+
+function parse_import($xml) {
+	global $cache, $need_update;
+	$url = (string)($xml->attributes()->url);
+	if( !$url ) {
+		msg("Unable to parse bogus <Import/> node", true);
+		msg($xml->asXML(), true);
+		return false;
+	}
+	$import_id = (string)$xml;
+	msg("Parsing import of '$import_id' from $url...");
+	$import = new SimpleXMLElement($url, 0, true);
+	$result = find_server($import, $import_id);
+	if( $result ) {
+		$old_version = $cache["import.".$import_id];
+		$cache["import.".$import_id] = (string)$result->attributes()->version;
+		if( $old_version != $cache["import.".$import_id] ) {
+			msg("Got new version of $import_id, need update...");
+			$need_update = true;
+		} else {
+			msg("Version of $import_id matches cache, not updating");
+		}
+	}
+	return $result;
+}
+
+function import_imports($base) {
+	$result = new SimpleXMLElement($base->asXML());
+	$imports = $base->Import;
+	unset($result->Import);
+	if( is_array($imports) ) {
+		foreach($imports as $key => $val) {
+			$import = parse_import($val);
+			if( $import ) {
+				append_children($result, $import);
+			}
+		}
+	} else {
+		$import = parse_import($imports);
+		if( $import ) {
+			append_children($result, $import);
+		}
+	}
+	return $result;
+}
+if( $base->Import ) {
+	msg("Handling imports...");
+	$base = import_imports($base);
+	print_r($base);
+}
 
 // check if we need to change
-if( ($got_version = (string)$base->attributes()->version) != $cache["version"]) {
+if( ($got_version = (string)($base->attributes()->version)) != $cache["version"]) {
 	msg("Identified new pack version $got_version");
-	// TODO: update pack
+	$need_update = true;
 } else {
 	msg("Pack version $got_version matches current rev, not updating");
+}
+
+// perform update if necessary
+if( $need_update ) {
+	msg("Performing update...");
+	// TODO: actually update :)
 }
 
 // flush cache to disk
@@ -93,6 +163,9 @@ $cache_json = json_encode($cache);
 file_put_contents($mcu_cache_filename, $cache_json);
 
 // TODO: back up existing world data
+if( file_exists("server.properties") ) {
+
+}
 
 // start server
 if( $server_autostart ) {
